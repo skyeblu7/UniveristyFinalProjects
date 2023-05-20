ab9a090026112446ed5830972166b883435fb391
skyeblu7 <skyeblu7@protonmail.com>
2023-05-20 14:45:11 -0500

    moved CS411 project to new repo

--- CS411FinalProjectDatabase/doc/final_demo_video.md
+++
@@ -1,2 +0,0 @@
-## Final Demo Video Google Drive Link:
-https://drive.google.com/file/d/1tGlT6tvv8vUeEvF-dp2sFY3bGmUUP36J/view?usp=sharing
--- CS411FinalProjectDatabase/sql/update_images.sql
+++
@@ -1,2 +0,0 @@
-UPDATE credit_cards SET image_url = 'https://creditcards.chase.com/K-Marketplace/images/cardart/freedom_unlimited_card_alt.png' WHERE image_url LIKE 'data:image/%';
-UPDATE credit_cards SET image_url = 'https://creditcards.chase.com/K-Marketplace/images/cardart/freedom_unlimited_card_alt.png' WHERE image_url NOT LIKE '%.png' AND image_url NOT LIKE '%.jpeg' AND image_url NOT LIKE '%.jpg';
